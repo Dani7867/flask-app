f31aea0e051eacbcfd9e43475697b66e0a126cdf
Dani7867 <119788393+Dani7867@users.noreply.github.com>
2024-12-13 10:01:57 +0530

    Update app.py on 2024-12-13T10:01:57+05:30

--- app.py
+++ app.py
@@ -463,3 +463,4 @@ print("Connecting to database...")
 if __name__ == "__main__":
     print("App is running")
 # Commit made on 2025-01-31T13:28:58+05:30
+# Commit made on 2024-12-13T10:01:57+05:30
